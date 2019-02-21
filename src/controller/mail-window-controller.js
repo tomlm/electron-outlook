7f3fdc5b9923093a37fcf5153687a8d8d093a548
Tom Laird-McConnell <tomlm@microsoft.com>
2019-02-21 10:19:48 -0800

    change from outlook -> office365

--- src/controller/mail-window-controller.js
+++ src/controller/mail-window-controller.js
@@ -3,9 +3,10 @@ const settings = require('electron-settings')
 const CssInjector = require('../js/css-injector')
 const path = require('path')
 
-const outlookUrl = 'https://outlook.live.com/mail'
-const deeplinkUrls = ['outlook.live.com/mail/deeplink', 'outlook.office365.com/mail/deeplink', 'outlook.office.com/mail/deeplink']
-const outlookUrls = ['outlook.live.com', 'outlook.office365.com', 'outlook.office.com']
+const outlookUrl = 'https://mail.office365.com/mail'
+const deeplinkUrls = ['outlook-sdf.office.com/mail/deeplink', 'outlook.office365.com/mail/deeplink', 'outlook.office.com/mail/deeplink']
+const outlookUrls = ['outlook-sdf.office.com', 'outlook.office.com', 'outlook.office365.com',
+'outlook.office365.com/calendar', 'outlook.office365.com/people', 'outlook.office365.com/files']
 
 class MailWindowController {
     constructor() {
@@ -67,66 +68,66 @@ class MailWindowController {
     }
 
     addUnreadNumberObserver() {
-        this.win.webContents.executeJavaScript(`
-            setTimeout(() => {
-                let unreadSpan = document.querySelector('._2iKri0mE1PM9vmRn--wKyI');
-                require('electron').ipcRenderer.send('updateUnread', unreadSpan.hasChildNodes());
+        // this.win.webContents.executeJavaScript(`
+        //     setTimeout(() => {
+        //         let unreadSpan = document.querySelector('._2iKri0mE1PM9vmRn--wKyI');
+        //         require('electron').ipcRenderer.send('updateUnread', unreadSpan.hasChildNodes());
 
-                let observer = new MutationObserver(mutations => {
-                    mutations.forEach(mutation => {
-                        console.log('Observer Changed.');
-                        require('electron').ipcRenderer.send('updateUnread', unreadSpan.hasChildNodes());
+        //         let observer = new MutationObserver(mutations => {
+        //             mutations.forEach(mutation => {
+        //                 console.log('Observer Changed.');
+        //                 require('electron').ipcRenderer.send('updateUnread', unreadSpan.hasChildNodes());
 
-                        // Scrape messages and pop up a notification
-                        var messages = document.querySelectorAll('div[role="listbox"][aria-label="Message list"]');
-                        if (messages.length)
-                        {
-                            var unread = messages[0].querySelectorAll('div[aria-label^="Unread"]');
-                            var body = "";
-                            for (var i = 0; i < unread.length; i++)
-                            {
-                                if (body.length)
-                                {
-                                    body += "\\n";
-                                }
-                                body += unread[i].getAttribute("aria-label").substring(7, 127);
-                            }
-                            if (unread.length)
-                            {
-                                var notification = new Notification(unread.length + " New Messages", {
-                                    body: body,
-                                    icon: "assets/outlook_linux_black.png"
-                                });
-                                notification.onclick = () => {
-                                    require('electron').ipcRenderer.send('show');
-                                };
-                            }
-                        }
-                    });
-                });
+        //                 // Scrape messages and pop up a notification
+        //                 var messages = document.querySelectorAll('div[role="listbox"][aria-label="Message list"]');
+        //                 if (messages.length)
+        //                 {
+        //                     var unread = messages[0].querySelectorAll('div[aria-label^="Unread"]');
+        //                     var body = "";
+        //                     for (var i = 0; i < unread.length; i++)
+        //                     {
+        //                         if (body.length)
+        //                         {
+        //                             body += "\\n";
+        //                         }
+        //                         body += unread[i].getAttribute("aria-label").substring(7, 127);
+        //                     }
+        //                     if (unread.length)
+        //                     {
+        //                         var notification = new Notification(unread.length + " New Messages", {
+        //                             body: body,
+        //                             icon: "assets/outlook_linux_black.png"
+        //                         });
+        //                         notification.onclick = () => {
+        //                             require('electron').ipcRenderer.send('show');
+        //                         };
+        //                     }
+        //                 }
+        //             });
+        //         });
             
-                observer.observe(unreadSpan, {childList: true});
+        //         observer.observe(unreadSpan, {childList: true});
 
-                // If the div containing reminders gets taller we probably got a new
-                // reminder, so force the window to the top.
-                let reminders = document.getElementsByClassName("_1BWPyOkN5zNVyfbTDKK1gM");
-                let height = 0;
-                let reminderObserver = new MutationObserver(mutations => {
-                    mutations.forEach(mutation => {
-                        if (reminders[0].clientHeight > height)
-                        {
-                            require('electron').ipcRenderer.send('show');
-                        }
-                        height = reminders[0].clientHeight;
-                    });
-                });
+        //         // If the div containing reminders gets taller we probably got a new
+        //         // reminder, so force the window to the top.
+        //         let reminders = document.getElementsByClassName("_1BWPyOkN5zNVyfbTDKK1gM");
+        //         let height = 0;
+        //         let reminderObserver = new MutationObserver(mutations => {
+        //             mutations.forEach(mutation => {
+        //                 if (reminders[0].clientHeight > height)
+        //                 {
+        //                     require('electron').ipcRenderer.send('show');
+        //                 }
+        //                 height = reminders[0].clientHeight;
+        //             });
+        //         });
 
-                if (reminders.length) {
-                    reminderObserver.observe(reminders[0], { childList: true });
-                }
+        //         if (reminders.length) {
+        //             reminderObserver.observe(reminders[0], { childList: true });
+        //         }
 
-            }, 10000);
-        `)
+        //     }, 10000);
+        // `)
     }
 
     toggleWindow() {
